7a4b4ae195e7f633a7da122398cb436617fa2b29
Alexandr <ale906206@yzndex.ru>
2025-07-13 03:33:34 +0300

    update: webhook config

--- src/main/java/com/elessarov/telegram_bot_spring_boot_starter/config/DefaultUpdateHandlerConfiguration.java
+++ src/main/java/com/elessarov/telegram_bot_spring_boot_starter/config/DefaultUpdateHandlerConfiguration.java
@@ -4,23 +4,29 @@ import com.elessarov.telegram_bot_spring_boot_starter.bot.UpdateHandler;
 import com.elessarov.telegram_bot_spring_boot_starter.util.BotUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 @Configuration
-public class DefaultUpdateHandlerConfiguration {
+@ConditionalOnMissingBean(UpdateHandler.class)
+public class DefaultUpdateHandlerConfiguration implements InitializingBean {
     public static final String UPDATE_HANDLER_BEAN_MISSING = "Update received but your UpdateHandler bean is not configured";
 
     private final Logger log = LoggerFactory.getLogger(DefaultUpdateHandlerConfiguration.class);
 
     @Bean
-    @ConditionalOnMissingBean(UpdateHandler.class)
     public UpdateHandler noOpUpdateHandler() {
         return update -> {
-            log.info(UPDATE_HANDLER_BEAN_MISSING);
+            log.warn(UPDATE_HANDLER_BEAN_MISSING);
             return new SendMessage(BotUtils.getChatId(update), UPDATE_HANDLER_BEAN_MISSING);
         };
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        log.warn("UpdateHandler bean was not configured, noOpUpdateHandler will be used");
     }
 }
